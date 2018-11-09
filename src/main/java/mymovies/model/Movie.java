f31141b77c6fa7901bcebfa7a0b7bff4d599e050
Mohamed Elkholy <mohamedtaher567@gmail.com>
2018-11-09 14:00:53 +0200

    Listing movies

---
+++ src/main/java/mymovies/model/Movie.java
@@ -0,0 +1,33 @@
+package mymovies.model;
+
+public class Movie {
+	String name;
+	String year;
+	String logoPath;
+	String language;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getLogoPath() {
+		return logoPath;
+	}
+	public void setLogoPath(String logoPath) {
+		this.logoPath = logoPath;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+}
