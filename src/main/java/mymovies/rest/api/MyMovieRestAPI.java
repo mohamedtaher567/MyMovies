b8f333db151cf1bf0e9615fc385ad728f4937baa
Mohamed Elkholy <mohamedtaher567@gmail.com>
2018-11-10 00:02:30 +0200

    support pagination initial using next/prev

--- src/main/java/mymovies/rest/api/MyMovieRestAPI.java
+++ src/main/java/mymovies/rest/api/MyMovieRestAPI.java
@@ -2,11 +2,11 @@ package mymovies.rest.api;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,13 +16,16 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import mymovies.tmdb.api.APIConnector;
 
 @Controller
+@RequestMapping("movies")
 public class MyMovieRestAPI {
 	@Autowired
 	APIConnector apiConnector;
 
-	@RequestMapping("movies")
-	public String index(Map<String, Object> model) throws UnirestException {
-		model.put("movies", apiConnector.listMovies(1));
+	@RequestMapping({"{page_number}", "/"})
+	public String index(Map<String, Object> model, @PathVariable("page_number")  Optional<Integer>  pageNumberParam) throws UnirestException {
+		Integer pageNumber = pageNumberParam.isPresent() ? pageNumberParam.get() : 1;
+		model.put("movies", apiConnector.listMovies(pageNumber));
+		model.put("pageNumber", pageNumber);
 		return "movies";
 	}
 
